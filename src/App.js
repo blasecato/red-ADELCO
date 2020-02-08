49481f4728ea7292f2c075d55750140c7b02b641
darian7 <yoj.cabrera@udla.edu.co>
2020-02-08 18:51:37 -0500

    feact(scenes/login)

--- src/App.js
+++ src/App.js
@@ -1,49 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './main.scss';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { useSelector } from 'react-redux'
 
-import Home from "../src/scenes/Home";
-import Login from "../src/scenes/Login/Login";
-import Users from "../src/scenes/Users/Users";
-import QueryUser from "../src/scenes/Users/QueryUser/QueryUser";
-import DeleteUpdateUser from "../src/scenes/Users/DeleteUpdateUser/DeleteUpdateUser";
-import RegisterUser from "../src/scenes/Users/RegisterUser/RegisterUser";
-import Profile from "../src/scenes/Users/Profile/Profile";
-import Coberturas from "../src/scenes/Coberturas/Coberturas";
-import CadenasProductivas from "../src/scenes/CadenasProductivas/CadenasProductivas";
-import Infraestructura from "../src/scenes/Infraestructura/Infraestructura";
-import Organizaciones from "../src/scenes/Organizaciones/Organizaciones";
-import Tipo from "../src/scenes/Infraestructura/Tipo/Tipo";
-import TipoCadena from "../src/scenes/CadenasProductivas/tipo/TipoCadena";
-import RegisterOrg from "../src/scenes/Organizaciones/components/RegisterOrg/RegisterOrg";
-import RegisterInfra from "../src/scenes/Infraestructura/components/RegisterInfra/RegisterInfra";
-import { Indicadores } from "../src/scenes/Indicadores/Indicadores";
+import { Private } from './scenes/Layout/Private/Private'
+import { Public } from './scenes/Layout/Public/Public'
 
+export const App = () => {
 
-function App() {
+  const { authentication } = useSelector(state => state.auth)
+
   return (
-    <div className="App">
-      <Router>
-        <Route path="/" exact component={Home} />
-        <Route path="/home" exact component={Home} />
-        <Route path="/login/" component={Login} />
-        <Route path="/users/" component={Users} />
-        <Route path="/queryuser/" component={QueryUser} />
-        <Route path="/deleteupdateuser/" component={DeleteUpdateUser} />
-        <Route path="/profile/" component={Profile} />
-        <Route path="/coberturas/" component={Coberturas} />
-        <Route path="/registeruser/" component={RegisterUser} />
-        <Route path="/cadenasproductivas/" component={CadenasProductivas} />
-        <Route path="/infraestructura/" component={Infraestructura} />
-        <Route path="/organizaciones/" component={Organizaciones} />
-        <Route path="/tipo/" component={Tipo} />
-        <Route path="/tipocadena/" component={TipoCadena} />
-        <Route path="/registerorg/" component={RegisterOrg} />
-        <Route path="/registerinfra/" component={RegisterInfra} />
-        <Route path="/indicadores/" component={Indicadores} />
-      </Router>
+    <div>
+      {!authentication && <Public />}
+      {authentication && <Private />}
     </div>
   );
 }
-
-export default App;
--- src/common/api.js
+++ src/common/api.js
@@ -1,46 +1,91 @@
-const apiUrl='http://localhost:3001'
+//import { apiUrl } from '../Config/Environments';
+import { Token } from './Storage/Token';
+import { store } from '../index'
+import { auth } from '../services/Auth/AuthActions';
+
+const apiUrl = 'http://localhost:3001'
 
 export class Api {
 
+  post(url, data, formData) {
+    let dataBody
 
-    post(url, data, header) {
-      let dataBody = JSON.stringify(data);
-  
-      return fetch(`${apiUrl}${url}`, {
-        method: 'POST',
-        headers: (header ? header : {
-          'Accept': 'application/json',
-          'Content-type': 'application/json'
-        }),
-        body: dataBody
+    if (formData) {
+      dataBody = new FormData();
+      Object.keys(data).map(key => {
+        if (!Array.isArray(data[key]))
+          dataBody.append(key, data[key]);
+        else
+          data[key].forEach(item => dataBody.append(key, item))
       })
-    }
-  
-    put(url, data, header) {
-      let isFormData = data instanceof FormData;
-  
-      return fetch(`${apiUrl}${url}`, {
-        method: 'PUT',
-        headers: (header ? header :
+    } else
+      dataBody = JSON.stringify(data);
+
+    return fetch(`${apiUrl}${url}`, {
+      method: 'POST',
+      headers: (formData ? {
+        'Authorization': `Bearer ${Token.getToken()}`
+      } : {
+          'Accept': 'application/json',
+          'Content-type': 'application/json',
+          'Authorization': `Bearer ${Token.getToken()}`
+        }),
+      body: dataBody
+    }).then(async response => {
+      if (response.status === 401) {
+        store.dispatch(auth.logout());
+        return response;
+      }
+      response.payload = await response.json()
+      return response;
+    }).catch(err => err)
+  }
+
+  put(url, data, header) {
+    let isFormData = data instanceof FormData;
+
+    return fetch(`${apiUrl}${url}`, {
+      method: 'PUT',
+      headers: (header ? header :
+        isFormData ?
+          { 'Authorization': `Bearer ${Token.getToken()}` }
+          :
           {
             'Accept': isFormData ? '' : 'application/json',
             'Content-type': isFormData ? '' : 'application/json',
+            'Authorization': `Bearer ${Token.getToken()}`
           }
-        ),
-        body: isFormData ? data : JSON.stringify(data)
-      })
-    }
-  
-  
-    get(url, params) {
-      url = new URL(`${apiUrl}${url}`);
-      if (params)
-        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
-      return fetch(url, {
-        method: 'GET',
-        headers: {}
-      });
-    }
+      ),
+      body: isFormData ? data : JSON.stringify(data)
+    }).then(async response => {
+      if (response.status === 401) {
+        store.dispatch(auth.logout());
+        return response;
+      }
+      response.payload = await response.json()
+      return response;
+    }).catch(err => err)
   }
-  
-  export default new Api();
+
+  get(url, params) {
+    url = new URL(`${apiUrl}${url}`);
+    if (params)
+      Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+    return fetch(url, {
+      method: 'GET',
+      headers: {
+        'Authorization': `Bearer ${Token.getToken()}`
+      }
+    }).then(async response => {
+      if (response.status === 401) {
+        store.dispatch(auth.logout());
+        return response;
+      }
+      response.payload = await response.json()
+      return response;
+    }).catch(err => err)
+  }
+
+}
+
+export default new Api();
