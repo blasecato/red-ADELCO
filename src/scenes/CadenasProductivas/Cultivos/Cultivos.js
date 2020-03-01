bf9e0127b5daaacfc1edefdc8d158890ed33402d
darian7 <yoj.cabrera@udla.edu.co>
2020-03-01 09:21:25 -0500

    refactor(cultivo)

--- src/scenes/CadenasProductivas/Cultivos/Cultivos.js
+++ src/scenes/CadenasProductivas/Cultivos/Cultivos.js
@@ -5,8 +5,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import LayoutHome from "../../../components/LayoutHome/LayoutHome";
 import { crop } from '../../../services/crop/cropActions';
-import { JsonToExcel } from 'react-json-excel';
-import { json2excel, excel2json } from 'js2excel';
+//import { JsonToExcel } from 'react-json-excel';
+//import { json2excel, excel2json } from 'js2excel';
 import exel from "../../../assets/image/excel.png";
 
 const { Search } = Input;
@@ -42,7 +42,7 @@ export const Cultivo = () => {
 		dispatch(crop.getCropsProducer())
 	}, [])
 
-	const handleToExel = () => {
+	/*const handleToExel = () => {
 		const jsonConvert = []
 		cropsProducer.forEach(item => {
 			jsonConvert.push({
@@ -59,21 +59,7 @@ export const Cultivo = () => {
 			formateDate: 'yyyy/mm/dd'
 		});
 
-	}
-
-	const className = 'class-name-for-style',
-		filename = 'Excel-file',
-		fields = {
-			"index": "Index",
-			"guid": "GUID"
-		},
-		style = {
-			padding: "5px"
-		},
-		data = [
-			{ index: 0, guid: 'asdf231234' },
-			{ index: 1, guid: 'wetr2343af' }
-		]
+	}*/
 
 	return (
 		<div className="queryuser">
@@ -86,7 +72,9 @@ export const Cultivo = () => {
 					<Link to="/registercultivos" className="btn-register">Registrar Cultivo</Link>
 				</div>
 				<div className="btn-exel">
-					<button className="btn-exel--exel" onClick={handleToExel}><img className="img-excel" src={exel} /> Descargar</button>
+					<button className="btn-exel--exel"
+					//onClick={handleToExel}
+					><img className="img-excel" src={exel} /> Descargar</button>
 				</div>
 				<Card title={<p>Cultivos</p>}
 					extra={<Search
